d7833ab68c17449f4e0789e490f4a3d0d9ccbab6
Вячеслав Кузьминых <kuzminyh.va@edu.spbstu.ru>
2022-03-27 14:38:55 +0300

    q

--- TheBalda/main.c
+++ TheBalda/main.c
@@ -9,11 +9,18 @@ void demo_animation();
 void demo_colors();
 void demo_input();
 void about();
+void settings_menu();
+void difficulty_selection();
+void first_turn_selection();
+
+int difficult = 1; //сложность изначально "лёгкий"
+int turn = 1;
+
 
 int main()
 {
 	// Инициализируется консоль, скрывается курсор
-	con_init(80, 25);
+	con_init(80, 95);
 	show_cursor(0);
 
 	// Запуск главного меню
@@ -31,12 +38,12 @@ void main_menu()
 	const char* menu_items[] = {"Балда" ,"Игра", "Настройки", "Таблица рекордов", "О программе", "Выход"};
 	int menu_active_idx = 1;
 	int menu_items_count = sizeof(menu_items) / sizeof(menu_items[0]);
-	short clr_bg = CON_CLR_YELLOW;
-	short clr_bg_active = CON_CLR_MAGENTA_LIGHT;
+	short clr_bg = CON_CLR_BLACK;
+	short clr_bg_active = CON_CLR_RED;
 	short clr_font = CON_CLR_WHITE_LIGHT;
 	while (1)
 	{
-		int left = 30;
+		int left = 50;
 		int top = 2;
 		int b;
 
@@ -55,20 +62,20 @@ void main_menu()
 			gotoxy(left, top);
 			con_set_color(clr_font, btn_bg);
 
-			printf("********************");
+			printf("====================");
 			top++;
 			gotoxy(left, top);
-			printf("*                   ");
+			printf("|                   ");
 
 			gotoxy(left + 10 - strlen(menu_items[b]) / 2, top);
 			printf("%s", menu_items[b]);
 
 			con_set_color(clr_font, btn_bg);
 			gotoxy(left + 19, top);
-			printf("*");
+			printf("|");
 			top++;
 			gotoxy(left, top);
-			printf("********************");
+			printf("====================");
 			top += 2;
 		}
 
@@ -114,16 +121,16 @@ void main_menu()
 			}
 			else if (code == KEY_ENTER) // Нажата кнопка Enter
 			{
-				if (menu_active_idx == menu_items_count - 1) // Выбран последний пункт - это выход
+				if (menu_active_idx == 5) // Выбран последний пункт - это выход
 					return;
 
-				if (menu_active_idx == 0)
+				//if (menu_active_idx == 0)
 
-				if (menu_active_idx == 1)
+				if (menu_active_idx == 2) // Выбран пункт "настройки"
+					settings_menu();
+				//if (menu_active_idx == 1)
 
-				if (menu_active_idx == 2)
-
-				if (menu_active_idx == menu_items_count - 2)
+				//if (menu_active_idx == 3)
 
 				break;
 			}
@@ -140,3 +147,349 @@ void main_menu()
 	} // while(1)
 }
 
+void settings_menu()
+{
+	const char* menu_items[] = { "Настройки" ,"Сложность", "Первым ходит...", "Вернуться"};
+	int menu_active_idx = 1;
+	int menu_items_count = sizeof(menu_items) / sizeof(menu_items[0]);
+	short clr_bg = CON_CLR_BLACK;
+	short clr_bg_active = CON_CLR_RED;
+	short clr_font = CON_CLR_WHITE_LIGHT;
+	while (1)
+	{
+		int left = 50;
+		int top = 2;
+		int b;
+
+		// Заблокировать отрисовку
+		con_draw_lock();
+
+		// Очистка экрана
+		con_set_color(clr_font, clr_bg);
+		clrscr();
+		// Цикл отрисовывает кнопку
+		for (b = 0; b < menu_items_count; b++)
+		{
+			short btn_bg = clr_bg; // По умолчанию фон кнопки - как фон экрана
+			if (b == menu_active_idx)
+				btn_bg = clr_bg_active; // Если кнопка активна - то рисуется другим цветом
+			
+			gotoxy(left, top);
+			con_set_color(clr_font, btn_bg);
+
+			printf("====================");
+			top++;
+			gotoxy(left, top);
+			printf("|                   ");
+
+			gotoxy(left + 10 - strlen(menu_items[b]) / 2, top);
+			printf("%s", menu_items[b]);
+
+			con_set_color(clr_font, btn_bg);
+			gotoxy(left + 19, top);
+			printf("|");
+			top++;
+			gotoxy(left, top);
+			printf("====================");
+			top += 2;
+		}
+
+		// Данные подготовлены, вывести на экран
+		con_draw_release();
+
+
+		while (!key_is_pressed()) // Если пользователь нажимает кнопку
+		{
+			int code = key_pressed_code();
+			if (code == KEY_UP) // Если это стрелка вверх
+			{
+				// То переход к верхнему пункту (если это возможно)
+				if (menu_active_idx > 1)
+				{
+					menu_active_idx--;
+					break;
+				}
+				else
+				{
+					menu_active_idx = menu_items_count - 1;
+					break;
+				}
+			}
+			else if (code == KEY_DOWN) // Если стрелка вниз
+			{
+				// То переход к нижнему пункту (если это возможно)
+				if (menu_active_idx + 1 < menu_items_count)
+				{
+					menu_active_idx++;
+					break;
+				}
+				else
+				{
+					menu_active_idx = 1;
+					break;
+				}
+			}
+			else if (code == KEY_ESC || code == 'q' || code == 'Q' ||
+				code == (unsigned char)'й' || code == (unsigned char)'Й') // ESC или 'q' - выход
+			{
+				return;
+			}
+			else if (code == KEY_ENTER) // Нажата кнопка Enter
+			{
+				if (menu_active_idx == 3) // Выбран последний пункт - это выход
+					return;
+
+				if (menu_active_idx == 1)//Выбран пункт сложность
+					difficulty_selection();
+
+				if (menu_active_idx == 2)//Выбран пункт Первый ход
+					first_turn_selection();
+				
+				break;
+			}
+
+
+			pause(40); // Небольная пауза (чтобы не загружать процессор)
+		} // while (!key_is_pressed())
+
+
+		// "Съедается" оставшийся ввод
+		while (key_is_pressed())
+			key_pressed_code();
+
+	} // while(1)
+}
+
+
+void difficulty_selection()
+{
+	const char* menu_items[] = { "Сложность" ,"Лёгкая", "Средняя", "Сложная" ,"Вернуться"};
+	int menu_active_idx = 1;
+	int menu_items_count = sizeof(menu_items) / sizeof(menu_items[0]);
+	short clr_bg = CON_CLR_BLACK;
+	short clr_bg_active = CON_CLR_RED;
+	short clr_font = CON_CLR_WHITE_LIGHT;
+	short clr_bg_chosen = CON_CLR_RED_LIGHT;
+	while (1)
+	{
+		int left = 50;
+		int top = 2;
+		int b;
+
+		// Заблокировать отрисовку
+		con_draw_lock();
+
+		// Очистка экрана
+		con_set_color(clr_font, clr_bg);
+		clrscr();
+		// Цикл отрисовывает кнопку
+		for (b = 0; b < menu_items_count; b++)
+		{
+			short btn_bg = clr_bg; // По умолчанию фон кнопки - как фон экрана
+			if (b == difficult)
+				btn_bg = clr_bg_chosen;//Кнопка не активна и это текущая сложность
+			if (b == menu_active_idx)
+				btn_bg = clr_bg_active; // Если кнопка активна - то рисуется другим цветом
+
+			gotoxy(left, top);
+			con_set_color(clr_font, btn_bg);
+
+			printf("====================");
+			top++;
+			gotoxy(left, top);
+			printf("|                   ");
+
+			gotoxy(left + 10 - strlen(menu_items[b]) / 2, top);
+			printf("%s", menu_items[b]);
+
+			con_set_color(clr_font, btn_bg);
+			gotoxy(left + 19, top);
+			printf("|");
+			top++;
+			gotoxy(left, top);
+			printf("====================");
+			top += 2;
+		}
+
+		// Данные подготовлены, вывести на экран
+		con_draw_release();
+
+
+		while (!key_is_pressed()) // Если пользователь нажимает кнопку
+		{
+			int code = key_pressed_code();
+			if (code == KEY_UP) // Если это стрелка вверх
+			{
+				// То переход к верхнему пункту (если это возможно)
+				if (menu_active_idx > 1)
+				{
+					menu_active_idx--;
+					break;
+				}
+				else
+				{
+					menu_active_idx = menu_items_count - 1;
+					break;
+				}
+			}
+			else if (code == KEY_DOWN) // Если стрелка вниз
+			{
+				// То переход к нижнему пункту (если это возможно)
+				if (menu_active_idx + 1 < menu_items_count)
+				{
+					menu_active_idx++;
+					break;
+				}
+				else
+				{
+					menu_active_idx = 1;
+					break;
+				}
+			}
+			else if (code == KEY_ESC || code == 'q' || code == 'Q' ||
+				code == (unsigned char)'й' || code == (unsigned char)'Й') // ESC или 'q' - выход
+			{
+				return;
+			}
+			else if (code == KEY_ENTER) // Нажата кнопка Enter
+			{
+				if (menu_active_idx == 4) // Выбран последний пункт - это выход
+					return;
+
+				if (menu_active_idx == 1)//Лёгкая сложность
+					difficult = 1;
+
+				if (menu_active_idx == 2)//Средняя сложность
+					difficult = 2;
+
+				if (menu_active_idx == 3)//Сложная
+					difficult = 3;
+				break;
+			}
+
+
+			pause(40); // Небольная пауза (чтобы не загружать процессор)
+		} // while (!key_is_pressed())
+
+
+		// "Съедается" оставшийся ввод
+		while (key_is_pressed())
+			key_pressed_code();
+
+	} // while(1)
+}
+
+void first_turn_selection() {
+	const char* menu_items[] = { "Первым ходит..." ,"Человек", "Компьютер" ,"Вернуться" };
+	int menu_active_idx = 1;
+	int menu_items_count = sizeof(menu_items) / sizeof(menu_items[0]);
+	short clr_bg = CON_CLR_BLACK;
+	short clr_bg_active = CON_CLR_RED;
+	short clr_font = CON_CLR_WHITE_LIGHT;
+	short clr_bg_chosen = CON_CLR_RED_LIGHT;
+	while (1)
+	{
+		int left = 50;
+		int top = 2;
+		int b;
+
+		// Заблокировать отрисовку
+		con_draw_lock();
+
+		// Очистка экрана
+		con_set_color(clr_font, clr_bg);
+		clrscr();
+		// Цикл отрисовывает кнопку
+		for (b = 0; b < menu_items_count; b++)
+		{
+			short btn_bg = clr_bg; // По умолчанию фон кнопки - как фон экрана
+			if (b == turn)
+				btn_bg = clr_bg_chosen;//Кнопка не активна и это текущая сложность
+			if (b == menu_active_idx)
+				btn_bg = clr_bg_active; // Если кнопка активна - то рисуется другим цветом
+
+			gotoxy(left, top);
+			con_set_color(clr_font, btn_bg);
+
+			printf("====================");
+			top++;
+			gotoxy(left, top);
+			printf("|                   ");
+
+			gotoxy(left + 10 - strlen(menu_items[b]) / 2, top);
+			printf("%s", menu_items[b]);
+
+			con_set_color(clr_font, btn_bg);
+			gotoxy(left + 19, top);
+			printf("|");
+			top++;
+			gotoxy(left, top);
+			printf("====================");
+			top += 2;
+		}
+
+		// Данные подготовлены, вывести на экран
+		con_draw_release();
+
+
+		while (!key_is_pressed()) // Если пользователь нажимает кнопку
+		{
+			int code = key_pressed_code();
+			if (code == KEY_UP) // Если это стрелка вверх
+			{
+				// То переход к верхнему пункту (если это возможно)
+				if (menu_active_idx > 1)
+				{
+					menu_active_idx--;
+					break;
+				}
+				else
+				{
+					menu_active_idx = menu_items_count - 1;
+					break;
+				}
+			}
+			else if (code == KEY_DOWN) // Если стрелка вниз
+			{
+				// То переход к нижнему пункту (если это возможно)
+				if (menu_active_idx + 1 < menu_items_count)
+				{
+					menu_active_idx++;
+					break;
+				}
+				else
+				{
+					menu_active_idx = 1;
+					break;
+				}
+			}
+			else if (code == KEY_ESC || code == 'q' || code == 'Q' ||
+				code == (unsigned char)'й' || code == (unsigned char)'Й') // ESC или 'q' - выход
+			{
+				return;
+			}
+			else if (code == KEY_ENTER) // Нажата кнопка Enter
+			{
+				if (menu_active_idx == 3) // Выбран последний пункт - это выход
+					return;
+
+				if (menu_active_idx == 1)//Человек ходит первым
+					turn = 1;
+
+				if (menu_active_idx == 2)//Компьютер ходит первым
+					turn = 2;
+
+				break;
+			}
+
+
+			pause(40); // Небольная пауза (чтобы не загружать процессор)
+		} // while (!key_is_pressed())
+
+
+		// "Съедается" оставшийся ввод
+		while (key_is_pressed())
+			key_pressed_code();
+
+	} // while(1)
+}
